a6f8b00598843350e8822924961747ad1df5e851
alamm01 <102335347+alamm01@users.noreply.github.com>
2023-11-26 09:49:29 -0600

    added the startup files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const routes = require("./routes")
+
+const path = require("path")
+
+
+// initialize the application
+const app = express()
+
+const PORT = 3001
+
+
+
+app.use(express.static("public"));
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+
+app.use('/api', routes)
+
+
+// use GET route for the first page
+
+app.get("/routes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
